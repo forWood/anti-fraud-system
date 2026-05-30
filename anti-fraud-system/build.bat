ddbe1eadfd5b18130d2f2773384ccd28385fafaf
forWood <weng_wude@163.com>
2026-05-30 10:49:21 +0800

    chore: 删除无用日志和Windows构建脚本

--- anti-fraud-system/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set JAVA_HOME=D:\tools\java\jdk8\zulu8.84.0.15-ca-jdk8.0.442-win_x64
-set PATH=%JAVA_HOME%\bin;D:\tools\maven\apache-maven-3.9.16\bin;%PATH%
-cd /d e:\workspace\codebuddy\fxsb\anti-fraud-system
-echo JAVA_HOME=%JAVA_HOME%
-echo.
-call mvn clean package -DskipTests -T 4
-exit /b %ERRORLEVEL%
--- anti-fraud-system/build_microservices.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set JAVA_HOME=D:\tools\java\jdk8\zulu8.84.0.15-ca-jdk8.0.442-win_x64
-set PATH=%JAVA_HOME%\bin;D:\tools\maven\apache-maven-3.9.16\bin;%PATH%
-cd /d e:\workspace\codebuddy\fxsb\anti-fraud-system
-call mvn package -DskipTests -pl anti-fraud-microservices/transaction-monitor,anti-fraud-microservices/alert-management,anti-fraud-microservices/case-management,anti-fraud-microservices/report-generation -am > mvn_microservices.log 2>&1
-exit /b %ERRORLEVEL%
